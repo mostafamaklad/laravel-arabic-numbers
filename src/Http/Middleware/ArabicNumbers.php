3d9edb13b82380b5d6f71617bffd88b78e8f5106
Mostafa Maklad <dev.mostafa.maklad@gmail.com>
2018-10-18 11:18:04 +0300

    remove unuseful call in transfrom

--- src/Http/Middleware/ArabicNumbers.php
+++ src/Http/Middleware/ArabicNumbers.php
@@ -33,7 +33,5 @@ class ArabicNumbers extends TransformsRequest
             $western = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
             return str_replace($eastern, $western, $value);
         }
-
-        return parent::transform($key, $value);
     }
 }
